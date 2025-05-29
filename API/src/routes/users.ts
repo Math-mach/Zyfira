cb8af4972bdcf59a765fbb7ead3313c21b1c7eac
Matheus Bispo Machtura <math.machtura@gmail.com>
2025-05-29 15:55:29 -0300

    feat: add new function in user router with middleware of auth

--- API/src/routes/users.ts
+++ API/src/routes/users.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
-import { login, logout, register } from "../controllers/userController";
+import {
+    login,
+    logout,
+    register,
+    getUserProfile,
+    updateUserProfile,
+} from "../controllers/userController";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+
+router.use(authMiddleware);
+
 router.post("/logout", logout);
+router.get("/me", getUserProfile);
+router.put("/me", updateUserProfile);
 
 export default router;
