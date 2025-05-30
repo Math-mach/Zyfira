049b0f080dc4208f26cb86ddaddc099b90f4a53f
Matheus Bispo Machtura <math.machtura@gmail.com>
2025-05-30 07:46:13 -0300

    feat: add router in index API

--- API/src/index.ts
+++ API/src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-import userRoutes from "./routes/users";
+import routes from "./routes";
 
 const app = express();
 
@@ -12,6 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", userRoutes);
+app.use("/api", routes);
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
