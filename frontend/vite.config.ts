e9ebd18a99578f86fcadf32ea0f29179abba3cae
Matheus Bispo Machtura <math.machtura@gmail.com>
2025-09-15 17:01:20 -0300

    feat: Install shadcn UI library

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,17 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite"
+
 
 dotenv.config({ path: path.resolve(__dirname, "../API/.env") });
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     server: {
         proxy: {
             "/api": {
                 target: `http://localhost:${process.env.PORT}/`,
                 changeOrigin: true,
             },
+        },
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
         },
     },
 });
